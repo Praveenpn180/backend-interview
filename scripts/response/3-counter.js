7b3aea957a775e7bdbe2aee1ea13a6c5be10dc11
anmol-shazab <anmol@shazab.in>
2024-04-14 19:51:56 +0530

    Move code to respective scripts.

---
+++ scripts/response/3-counter.js
@@ -0,0 +1,31 @@
+export const createCounter = (initialValue = 0) => {
+  let count = initialValue;
+  const increment = () => {
+    count++;
+    return count;
+  };
+  const decrement = () => {
+    count--;
+    return count;
+  };
+  const setValue = (newValue) => {
+    count = newValue;
+    return count;
+  };
+  const getValue = () => {
+    return count;
+  };
+  return {
+    increment,
+    decrement,
+    setValue,
+    getValue,
+  };
+};
+
+const counter = createCounter(5);
+
+console.log(counter.getValue()); // get counter values
+console.log(counter.increment()); // increment value
+console.log(counter.decrement()); // decrement value
+console.log(counter.setValue(10)); // set counter value
